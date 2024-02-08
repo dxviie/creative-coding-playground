70b9ad6628579d42aebab360da19c805ee3d4e28
david <david@d17e.dev>
2024-02-08 16:05:32 +0100

    added shadcn + deps + avatar component

---
+++ ccp-svelte/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.pcss';
+    import * as Avatar from "$lib/components/ui/avatar";
+</script>
+
+<Avatar.Root>
+    <Avatar.Image src="https://avatars.githubusercontent.com/u/42746902" />
+</Avatar.Root>
+<slot />
