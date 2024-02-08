74a9875a7669de857888c5a9f6db29315dfd2b50
david <david@d17e.dev>
2024-02-08 14:17:07 +0100

    testing p5 sketch integration

---
+++ ccp-svelte/src/lib/components/TestSketch.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+
+    let canvas : HTMLElement;
+
+    let p5;
+
+    onMount(async () => {
+        // check if we're in the browser
+        // if (typeof window === 'undefined') return;
+        // dynamically load p5
+        p5 = await import('p5');
+        console.log('p5 loaded!')
+        console.log(p5)
+        // create a new p5 instance
+        new p5.default((p) => {
+            p.setup = () => {
+                p.createCanvas(400, 400);
+                p.background(0);
+            };
+            p.draw = () => {
+                p.fill(255);
+                p.ellipse(p.mouseX, p.mouseY, 50, 50);
+            };
+        }, canvas);
+    });
+
+</script>
+
+<div bind:this={canvas}>
+
+</div>
