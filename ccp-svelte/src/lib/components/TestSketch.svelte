6545b335efb157818e1a75404c1fb73c0fe3c905
david <david@d17e.dev>
2024-02-08 14:39:10 +0100

    sketch code extracted

--- ccp-svelte/src/lib/components/TestSketch.svelte
+++ ccp-svelte/src/lib/components/TestSketch.svelte
@@ -1,33 +1,22 @@
 <script lang="ts">
     import { onMount } from 'svelte';
+    import type p5 from 'p5';
 
+    export let sketch : (p: p5) => void;
 
     let canvas : HTMLElement;
 
-    let p5;
-
     onMount(async () => {
         // check if we're in the browser
-        // if (typeof window === 'undefined') return;
+        if (typeof window === 'undefined') return;
         // dynamically load p5
-        p5 = await import('p5');
+        let p5 = await import('p5');
         console.log('p5 loaded!')
         console.log(p5)
         // create a new p5 instance
-        new p5.default((p) => {
-            p.setup = () => {
-                p.createCanvas(400, 400);
-                p.background(0);
-            };
-            p.draw = () => {
-                p.fill(255);
-                p.ellipse(p.mouseX, p.mouseY, 50, 50);
-            };
-        }, canvas);
+        new p5.default(sketch, canvas);
     });
 
 </script>
 
-<div bind:this={canvas}>
-
-</div>
+<div bind:this={canvas}></div>
--- ccp-svelte/src/routes/+page.svelte
+++ ccp-svelte/src/routes/+page.svelte
@@ -1,8 +1,20 @@
-<script>
+<script lang="ts">
     import TestSketch from "$lib/components/TestSketch.svelte";
+    import type p5 from 'p5';
+
+    let s = (p : p5) => {
+        p.setup = () => {
+            p.createCanvas(400, 400);
+            p.background(0);
+        };
+        p.draw = () => {
+            p.fill(255);
+            p.ellipse(p.mouseX, p.mouseY, 50, 50);
+        };
+    };
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<TestSketch />
+<TestSketch sketch={s} />
 
